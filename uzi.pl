0ccb1a537571c3dd45f7e30633520fa6ce397c1e
Wiktor <wikii122@gmail.com>
2015-01-11 13:38:02 +0100

    Password generation works

--- uzi.pl
+++ uzi.pl
@@ -6,26 +6,57 @@
 # Refer to README file for project description.
 #
 # TODO:
-# + console mode
+# - console mode
+#   - create user
+#   - choose free uid
+#   + generate random password
+#   - copy home dir from template
+#   - store user data in root's catalogue
+#   - check if uid is taken
+#   - change user group
+#   - delete user
 # - interactive mode
+#   - create user
+#   - choose free uid
+#   - generate random password
+#   - copy home dir from template
+#   - store user data in root's catalogue
+#   - check if uid is taken
+#   - change user group
+#   - delete user
 
 use warnings;
 use strict;
 use v5.10;
+use Crypt::RandPasswd;
 
 sub launch {
-	say "GUI"
+	say "GUI";
+	exit 0;
 }
 
-my $help = "None yet";
+my $help = <<END;
+	Usage:
+		uzi.pl - start in interactive mode.
+		uzi.pl [option] - execute barch option.
 
-my $command = shift(@ARGV) or launch() and exit 0;
+	Help:
+		create - creates user
+		modify - modifies user
+		delete - deletes user
+		check - checks uid or creates new password.
+		interactive - start GUI
+END
+
+my $command = shift or die $help;
 
 if ($command eq "-h" or $command eq "--help") {
 	say $help;
-} else {
-	my $args = join(' ', @ARGV);
-	my $response = `useradd $args 2>&1`;
-	$response =~ s/useradd/uzi.pl/g;
-	say $response;
+} elsif ($command eq "check") {
+	my $command = shift(@ARGV) or die $help;
+	if ($command eq "password") {
+		say Crypt::RandPasswd->chars(8, 20);
+	}
+} elsif ($command eq "interactive") {
+	launch;
 }
